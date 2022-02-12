34c85017ae2b7b812eba18057ae116f18794fe4e
Mukeshgaur12 <yogeshgaur1995@gmail.com>
2022-02-12 17:14:45 +0530

    done

---
+++ pages/_app.jsx
@@ -0,0 +1,7 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../styles/NavBar.css"
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
